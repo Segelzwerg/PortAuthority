634df6da8ee59d5fb4649fc3ab006d43f0c526ff
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2025-08-02 22:01:13 +0200

    Register `Application` model with customized admin interface in `registry` app.

--- registry/admin.py
+++ registry/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Application
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    """
+    Admin interface for Application model.
+    
+    Provides a clean interface for managing applications in the Django admin.
+    """
+    list_display = ('protocol', 'url', 'port', 'full_address')
+    list_filter = ('protocol',)
+    search_fields = ('url', 'port')
+    ordering = ('protocol', 'url', 'port')
+
+    fieldsets = (
+        ('Application Details', {
+            'fields': ('protocol', 'url', 'port')
+        }),
+    )
+
+    def full_address(self, obj):
+        """Display the full address in the admin list view."""
+        return obj.full_address
+
+    full_address.short_description = 'Full Address'
