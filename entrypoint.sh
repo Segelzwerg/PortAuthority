391aa031352cd7a2b66b6e1a9c4ff0b27e0e06be
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2025-08-04 23:12:27 +0200

    ADD: entry script

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "🔁 Running migrations..."
+django-admin migrate --settings=$DJANGO_SETTINGS_MODULE --noinput
+
+echo "📦 Collecting static files..."
+django-admin collectstatic --settings=$DJANGO_SETTINGS_MODULE --noinput
+
+echo "🌍 Compiling translation messages..."
+django-admin compilemessages
+
+echo "🚀 Starting Gunicorn..."
+python -m gunicorn --bind=0.0.0.0 --timeout 600 portauthority.wsgi
