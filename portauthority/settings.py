6d0bf8e586959bdc7ab7cc3d8ae934b9a766763d
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2025-08-06 22:01:33 +0200

    FIX: static collection

--- portauthority/settings.py
+++ portauthority/settings.py
@@ -122,6 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]  # collect from
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 STATIC_ROOT = "staticfiles"
 
 # Default primary key field type
